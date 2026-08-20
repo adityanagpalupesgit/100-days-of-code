d9281526d33ada6fb93cd5ba4a7c1cb6a683f462
aditya-nagpal-opium <adityanagpal678@gmail.com>
2026-08-20 15:39:21 +0530

    initial commit

---
+++ day1.c
@@ -0,0 +1,12 @@
+      #include <stdio.h>
+
+        int main()
+        {
+            int num1,num2;
+            printf ("what is your first number? \n");
+            scanf ("%d", &num1);
+            printf ("what is your second number? \n");
+            scanf ("%d", &num2);
+            printf("the sum of your numbers are %d", num1 + num2);
+            return 0;
+        }
